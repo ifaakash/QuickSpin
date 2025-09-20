5ab0ec686bf62ce4d77a107521e79da2d5b4c206
ifaakash <aakashc.appregis@gmail.com>
2025-09-21 02:54:55 +0530

    fix(instance): Provide required variables in `variables.tf` for instance

--- IaC/main.tf
+++ IaC/main.tf
@@ -10,15 +10,12 @@ module "networking" {
   default_tags         = var.default_tags
 }
 
-
 module "instance" {
   source               = "git::https://github.com/ifaakash/Terraform//EC2?ref=main"
   prefix               = var.prefix
-  vpc_cidr             = var.vpc_cidr
-  subnet_cidr          = var.subnet_cidr
-  enable_dns_hostnames = var.enable_dns_hostnames
-  enable_dns_support   = var.enable_dns_support
-  user_ip              = var.user_ip
-  is_public            = var.is_public
+  ami_id               = var.ami_id
+  instance_type        = var.instance_type
+  network_interface_id = module.networking.network_interface_id
+  security_group_ids   = [module.networking.security_group_id]
   default_tags         = var.default_tags
 }
--- IaC/variables.tf
+++ IaC/variables.tf
@@ -4,6 +4,8 @@ variable "prefix" {
   default     = "quickspin"
 }
 
+##################### NETWORKING #####################
+
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
@@ -33,11 +35,34 @@ variable "is_public" {
   type        = bool
 }
 
+##################### INSTANCE #####################
+
 variable "user_ip" {
   description = "User's IP address"
   type        = string
 }
 
+variable "instance_type" {
+  description = "Instance type for EC2 instance"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+}
+
+variable "network_interface_id" {
+  description = "Network interface ID for EC2 instance"
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "Security group IDs for EC2 instance"
+  type        = list(string)
+}
+
+##################### DEFAULT TAGS #####################
 variable "default_tags" {
   description = "Default tags for resources deployment"
   type        = map(string)
