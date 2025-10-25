c57a3f615d5431250b889f9e793e1757c0b77c1b
ifaakash <aakashc.appregis@gmail.com>
2025-10-26 00:16:36 +0530

    refactor(terraform): Remove the `output.tf` file

--- IaC/output.tf
+++ IaC/output.tf
@@ -1,13 +1,13 @@
-output "vpc_id" {
-  description = "VPC ID being used by QuickSpin"
-  value       = module.ec2_stack.vpc_id
-}
+# output "vpc_id" {
+#   description = "VPC ID being used by QuickSpin"
+#   value       = module.ec2_stack.vpc_id
+# }
 
-output "instance_id" {
-  description = "Instance ID being used by QuickSpin"
-  value       = module.ec2_stack.instance_id
-}
+# output "instance_id" {
+#   description = "Instance ID being used by QuickSpin"
+#   value       = module.ec2_stack.instance_id
+# }
 
-output "security_group_id" {
-  value = module.networking.security_group_id
-}
+# output "security_group_id" {
+#   value = module.networking.security_group_id
+# }
