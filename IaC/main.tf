01d6f254df5c56e26a7d9b44ef6289d33acc9ca0
ifaakash <aakashc.appregis@gmail.com>
2025-11-01 22:54:02 +0530

    update(main.tf): Rename the NIC module to ENI + Debug EC2 Stack module

--- IaC/main.tf
+++ IaC/main.tf
@@ -22,7 +22,7 @@ module "iam" {
   instance_profile_name = "${var.prefix}-${var.instance_profile_name}"
 }
 
-module "nic" {
+module "eni" {
   source = "git::https://github.com/ifaakash/Terraform//Networking//NIC?ref=main"
   prefix = var.prefix
 
@@ -36,21 +36,21 @@ module "nic" {
   default_tags = each.value.is_public ? merge({ "Name" = "${var.prefix}-public-eni-${each.key}" }, var.default_tags) : merge({ "Name" = "${var.prefix}-private-eni-${each.key}" }, var.default_tags)
 }
 
-# module "ec2_stack" {
-#   source   = "git::https://github.com/ifaakash/Terraform//EC2?ref=main"
-#   for_each = { for index, inst in var.instances : index => inst }
-#   prefix   = var.prefix
+module "ec2_stack" {
+  source   = "git::https://github.com/ifaakash/Terraform//EC2?ref=main"
+  for_each = { for index, inst in var.instances : index => inst }
+  prefix   = var.prefix
 
-#   ##################### INSTANCE #####################
+  ##################### INSTANCE #####################
 
-#   ami_id                = each.value.ami_id
-#   instance_type         = each.value.instance_type
-#   network_interface_id  = module.nic[each.key].id
-#   security_group_ids    = [module.networking.security_group_id]
-#   instance_profile_name = module.iam.instance_profile_name
-#   depends_on            = [module.networking, module.iam, module.nic]
-#   default_tags          = merge({ "Name" = "${var.prefix}-${each.value.is_public ? "public" : "private"}-instance" }, var.default_tags)
-# }
+  ami_id                = each.value.ami_id
+  instance_type         = each.value.instance_type
+  network_interface_id  = module.eni[each.key].eni
+  security_group_ids    = [module.networking.security_group_id]
+  instance_profile_name = module.iam.instance_profile_name
+  depends_on            = [module.networking, module.iam, module.eni]
+  default_tags          = merge({ "Name" = "${var.prefix}-${each.value.is_public ? "public" : "private"}-instance" }, var.default_tags)
+}
 
 /*
 "ami_id" : instance["ami"],
